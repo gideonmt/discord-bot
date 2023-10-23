8435eb78e2c421a8c6dda75c3ee1bc4ac3d7501f
gideon <gmarcustrask26@d-e.org>
2023-10-22 21:57:25 -0400

    add info to userinfo
    added roles and user id

--- commands/utility/serverinfo.js
+++ commands/utility/serverinfo.js
@@ -32,7 +32,6 @@ module.exports = {
 		const voiceChannels = interaction.guild.channels.cache.filter(channel => channel.type === 'GUILD_VOICE').size;
 		const textChannels = interaction.guild.channels.cache.filter(channel => channel.type === 'GUILD_TEXT').size;
 		const forumChannels = interaction.guild.channels.cache.filter(channel => channel.type === 'GUILD_NEWS').size;
-		const categories = interaction.guild.channels.cache.filter(channel => channel.type === 'GUILD_CATEGORY').size;
 
 		// features
 		const serverFeatures = interaction.guild.features.join(', ');
@@ -65,7 +64,7 @@ module.exports = {
 				{ name: 'Members', value: [`- Total: \`${totalCount}\``, `- Humans: \`${memberCount}\``, `- Bots: \`${botCount}\``, `- Online: \`${onlineCount}\``].join('\n'), inline: true },
 				{ name: 'Created', value: interaction.guild.createdAt.toLocaleString(), inline: true },
 				{ name: 'Security', value: `- Verification Level: \`${verificationLevel}\`\n- Content Filter: \`${contentFilter}\`\n- Default Notifications: \`${defaultNotifications}\`\n- Two-Factor Auth: \`${twoFactorAuth}\``, inline: true },
-				{ name: 'Channels', value: `- Total: \`${totalChannels}\`\n- Voice Channels: \`${voiceChannels}\`\n- Text Channels: \`${textChannels}\`\n- Forum Channels: \`${forumChannels}\`\n- Categories: \`${categories}\``, inline: true },
+				{ name: 'Channels', value: `- Total: \`${totalChannels}\`\n- Voice Channels: \`${voiceChannels}\`\n- Text Channels: \`${textChannels}\`\n- Forum Channels: \`${forumChannels}\``, inline: true },
 				{ name: 'Server Features', value: serverFeatures },
 				{ name: `Roles [${roles.size}]`, value: `${rolesInfo}` },
 				{ name: `Emojis [${totalEmojis}]`, value: emojisInfo, inline: !!emojisInfo2 },
--- commands/utility/userinfo.js
+++ commands/utility/userinfo.js
@@ -7,36 +7,49 @@ module.exports = {
 		.addUserOption(option => option.setName('target').setDescription('The member you want to get information about')),
 	async execute(interaction) {
 		const member = interaction.options.getMember('target') || interaction.member;
+
+		const roles = member.roles.cache
+			.filter((role) => role.name !== '@everyone')
+			.map((role) => `<@&${role.id}>`)
+			.join(', ');
+
 		const activities = member.presence?.activities || []
+
 		const embed = {
 			"author": {
-			  "name": member.user.tag,
-			  "icon_url": member.user.displayAvatarURL()
+				"name": member.user.tag,
+				"icon_url": member.user.displayAvatarURL()
 			},
 			"color": member.displayColor === "#000000" ? "#ffffff" : member.displayColor,
 			"description": activities.map((x, i) => `**${x.type}**: \`${x.name || "None"} : ${x.details || "None"} : ${x.state || "None"}\``).join("\n"),
 			"fields": [
-			  {
-				"name": "JoinedAt",
-				"value": member.joinedAt.toLocaleString(),
-				"inline": true
-			  },
-			  {
-				"name": "Account Created At",
-				"value": member.user.createdAt.toLocaleString(),
-				"inline": true
-			  },
-			  {
-				"name": "Common Information",
-				"value": [
-				  `Display Name: \`${member.displayName}\``,
-				  `Pending Member: \`${member.pending ? 'Yes' : 'No'}\``,
-				  `Booster: \`${member.premiumSince ? 'since ' + member.premiumSince.toLocaleString() : 'Nope'}\``,
-				].join("\n")
-			  }
+				{
+					"name": "Joined Server At",
+					"value": member.joinedAt.toLocaleString(),
+					"inline": true
+				},
+				{
+					"name": "Account Created At",
+					"value": member.user.createdAt.toLocaleString(),
+					"inline": true
+				},
+				{
+					"name": "Common Information",
+					"value": [
+						`Display Name: \`${member.displayName}\``,
+						`User ID: \`${member.user.id}\``,
+						`Pending Member: \`${member.pending ? 'Yes' : 'No'}\``,
+						`Booster: \`${member.premiumSince ? 'since ' + member.premiumSince.toLocaleString() : 'Nope'}\``,
+					].join("\n")
+				},
+				{
+					name: 'Roles',
+					value: roles,
+					inline: false,
+				}
 			]
-		  };
-		  
+		};
+
 		await interaction.reply({ embeds: [embed] });
 	},
 };
