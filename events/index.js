b36e1765093998a1b1508fd6e2bc8deaac096cb6
gideon <gmarcustrask26@d-e.org>
2023-10-26 21:06:12 -0400

    update event file names

--- events/index.js
+++ events/index.js
@@ -1,24 +1,24 @@
 const { Events } = require('discord.js');
 
 module.exports = (client) => {
-    const memberJoin = require('./memberJoin');
-    const memberLeave = require('./memberLeave');
-    const messageCreate = require('./messageCreate');
-    const messageReact = require('./messageReact');
+    const GuildMemberAdd = require('./GuildMemberAdd');
+    const GuildMemberRemove = require('./GuildMemberRemove');
+    const MessageCreate = require('./MessageCreate');
+    const MessageReactionAdd = require('./MessageReactionAdd');
 
     client.on(Events.GuildMemberAdd, async member => {
-        memberJoin.handleMemberJoin(member, client);
+        GuildMemberAdd.handleMemberJoin(member, client);
     });
 
     client.on(Events.GuildMemberRemove, async member => {
-        memberLeave.handleMemberLeave(member, client);
+        GuildMemberRemove.handleMemberLeave(member, client);
     });
 
     client.on(Events.MessageCreate, async message => {
-        messageCreate.handleMessageCreate(message, client);
+        MessageCreate.handleMessageCreate(message, client);
     });
 
     client.on(Events.MessageReactionAdd, async (reaction) => {
-        messageReact.handleStarboard(reaction, client);
+        MessageReactionAdd.handleStarboard(reaction, client);
     });
 }
