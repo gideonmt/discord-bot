a9caea8fc33073aa89c02a6688c4286f61a72499
gideon <gmarcustrask26@d-e.org>
2023-11-06 19:30:50 -0500

    use available port

--- website/server.js
+++ website/server.js
@@ -1,18 +1,34 @@
 module.exports = () => {
     const fs = require('fs');
-
     const express = require('express');
     const bodyParser = require('body-parser');
     const app = express();
-    const port = process.env.PORT || 3000;
+    let port = process.env.PORT || 3000;
 
     app.enable('trust proxy');
     app.set("etag", false);
     app.use(express.static(__dirname));
-
     app.use(bodyParser.json());
 
-    app.get('/api/settings', (res) => {
+    // Create a function to start the server and handle port conflicts
+    const startServer = () => {
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        }).on('error', (err) => {
+            if (err.code === 'EADDRINUSE') {
+                console.log(`Port ${port} is already in use. Trying the next available port...`);
+                port++;
+                startServer(); // Try the next port
+            } else {
+                console.error(err);
+            }
+        });
+    };
+
+    // Start the server with error handling
+    startServer();
+
+    app.get('/api/settings', (req, res) => {
         const data = fs.readFileSync('settings.json');
         const settings = JSON.parse(data);
         res.json(settings);
@@ -22,9 +38,5 @@ module.exports = () => {
         const updatedSettings = req.body;
         fs.writeFileSync('settings.json', JSON.stringify(updatedSettings, null, 2));
         res.json({ message: 'Settings updated successfully' });
-    });
-
-    app.listen(port, () => {
-        console.log(`Server is running on port ${port}`);
     });
 };
